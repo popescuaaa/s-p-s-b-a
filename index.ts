e5453e6e6da5c2f0721e373ac434efdd5924f5bd
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-10-10 18:32:09 +0300

    Resolved playground

--- index.ts
+++ index.ts
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 
-import { loadAllTests, readYaml } from "./src/system_utility/DataReader";
+import { loadAllTests } from "./src/system_utility/DataReader";
 import { Auction } from "./src/models/Auction";
-import fs from "fs";
+import Dynamic from "./src/solver/Dynamic";
 import Iterative from "./src/solver/Iterative";
 
-const data: Auction[] = loadAllTests("./assets");
-console.log(data[4].buyers);
-const solver = new Iterative();
-console.log(
-  solver.solve(
+const playground = () => {
+  const data: Auction[] = loadAllTests("./assets");
+  const iSolver = new Iterative();
+  const dSolver = new Dynamic();
+
+  const iResult = iSolver.solve(
     Object.entries(data[4].buyers).map((e) => e[1]),
     data[4].reservedPrice
-  )
-);
+  );
+
+  const dResult = dSolver.solve(
+    Object.entries(data[4].buyers).map((e) => e[1]),
+    data[4].reservedPrice
+  );
+
+  console.log("Dynamic version result", dResult);
+  console.log("Iterative version result", iResult);
+
+  console.log(dResult.bid === iResult.bid && dResult.name === iResult.name);
+};
+
+playground();
