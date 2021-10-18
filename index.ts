4b2376ae629fc9e0dd143afa3bab6c788bbbe3c2
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-10-18 17:58:45 +0300

    Added discussion after interview

--- index.ts
+++ index.ts
@@ -8,22 +8,13 @@ import Iterative from "./src/solver/Iterative";
 const playground = () => {
   const data: Auction[] = loadAllTests("./assets");
   const iSolver = new Iterative();
-  const dSolver = new Dynamic();
 
   const iResult = iSolver.solve(
     Object.entries(data[4].buyers).map((e) => e[1]),
     data[4].reservedPrice
   );
 
-  const dResult = dSolver.solve(
-    Object.entries(data[4].buyers).map((e) => e[1]),
-    data[4].reservedPrice
-  );
-
-  console.log("Dynamic version result", dResult);
   console.log("Iterative version result", iResult);
-
-  console.log(dResult.bid === iResult.bid && dResult.name === iResult.name);
 };
 
 playground();
--- src/solver/Iterative.ts
+++ src/solver/Iterative.ts
@@ -18,28 +18,75 @@ class Iterative implements Solver {
     );
 
     /* Sort bid in descending order using TimSort */
-    TimSort.sort(
-      maxBidsBuyers,
-      (mx0: MaxBidBuyer, mx1: MaxBidBuyer) => mx1.bid - mx0.bid
-    );
+    // TimSort.sort(
+    //   maxBidsBuyers,
+    //   (mx0: MaxBidBuyer, mx1: MaxBidBuyer) => mx1.bid - mx0.bid
+    // );
 
-    /* Empty result check for the specified case the max is -1*/
-    if (maxBidsBuyers[0].bid === -1) {
-      return EmptyResult;
-    }
+    /**
+     *  maxBid, preMaxBid
+     * [ 1, 2, 3, 4, 5, 6]
+     *
+     *
+     */
 
-    /* There are at least two buyers who listed the same bid in current Auction */
-    if (maxBidsBuyers[0].bid === maxBidsBuyers[1].bid) {
-      return EmptyResult;
-    }
+    const winner = maxBidsBuyers.reduce((b0, b1) => {
+      if (b0.bid <= b1.bid) {
+        return <MaxBidBuyer>{ name: b1.name, bid: b0.bid };
+      } else {
+        return <MaxBidBuyer>{ name: b0.name, bid: b1.bid };
+      }
+    });
 
-    return {
-      name: maxBidsBuyers[0].name,
-      bid:
-        maxBidsBuyers[1].bid <= reservedPrice
-          ? reservedPrice
-          : maxBidsBuyers[1].bid,
+    return <Result>{
+      name: winner.name,
+      bid: winner.bid <= reservedPrice ? reservedPrice : winner.bid,
     };
+
+    // // O(N)
+    // maxBidsBuyers.map((e) => {
+    //   if (e.bid >= maxBid) {
+    //     maxBid = e.bid;
+    //     buyerName = e.name;
+    //   }
+    // });
+    //
+    // // O(N)
+    // maxBidsBuyers.map((e) => {
+    //   if (e.bid !== maxBid && e.bid >= preMaxBid) {
+    //     preMaxBid = e.bid;
+    //   }
+    // });
+
+    // if (preMaxBid >= reservedPrice) {
+    //   return <Result>{
+    //     name: buyerName,
+    //     bid: preMaxBid,
+    //   };
+    // } else {
+    //   return <Result>{
+    //     name: buyerName,
+    //     bid: reservedPrice,
+    //   };
+    // }
+
+    // /* Empty result check for the specified case the max is -1*/
+    // if (maxBidsBuyers[0].bid === -1) {
+    //   return EmptyResult;
+    // }
+    //
+    // /* There are at least two buyers who listed the same bid in current Auction */
+    // if (maxBidsBuyers[0].bid === maxBidsBuyers[1].bid) {
+    //   return EmptyResult;
+    // }
+    //
+    // return {
+    //   name: maxBidsBuyers[0].name,
+    //   bid:
+    //     maxBidsBuyers[1].bid <= reservedPrice
+    //       ? reservedPrice
+    //       : maxBidsBuyers[1].bid,
+    // };
   }
 }
 
